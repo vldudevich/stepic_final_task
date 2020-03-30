7837936fd5d6c4a1638ad9d6198b5278651141cb
Vladislav <vladislav.dudevich@letmecode.org>
2020-03-30 18:33:49 +0300

    add check basket_product

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,6 @@ class LoginPageLocators():
     LOGIN_FORM_LINK = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM_LINK = (By.CSS_SELECTOR, "#register_form")
 
+class ProductPageLocators():
+    ADD_TO_BASKET = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    NAME_OF_PRODUCT = (By.CSS_SELECTOR, ".alertinner strong")
--- pages/login_page.py
+++ pages/login_page.py
@@ -9,7 +9,7 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        assert "ogin/" in self.browser.current_url, "Login url is not presented"
+        assert "login/" in self.browser.current_url, "Login url is not presented"
 
     def should_be_login_form(self):
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM_LINK), "Login form is not presented"
